a3208ada21484e37f21354ce025886d0921e1ca3
julia <j.kholstinina@yandex.ru>
2022-04-19 18:32:07 +0300

    diff util

---
+++ utils/src/main/java/com/example/utils/DiffCallback.kt
@@ -0,0 +1,30 @@
+package com.example.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.module.data.DataModel
+
+class DiffCallback (
+        private val oldList: List<DataModel>,
+        private val newList: List<DataModel>,
+): DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+        return oldItem == newItem
+    }
+}
